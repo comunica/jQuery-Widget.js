c5a03301f11fffba3c2745217fcc41b9e3f3072b
noahvsb <161894387+noahvsb@users.noreply.github.com>
2025-08-19 14:18:38 +0200

    Fixed querying 4 times when the executeOnLoad option is true

--- src/ldf-client-ui.js
+++ src/ldf-client-ui.js
@@ -536,8 +536,10 @@ if (typeof global.process === 'undefined')
         break;
       case 'executeOnLoad':
         this.$executeOnLoad.prop('checked', value).change();
-        if (value && !initialize)
+        if (value && !initialize && !this._alreadyExecutedOnLoad) {
+          this._alreadyExecutedOnLoad = true;
           this._startExecution();
+        }
         break;
       // Set the list of selectable queries
       case 'relevantQueries':
